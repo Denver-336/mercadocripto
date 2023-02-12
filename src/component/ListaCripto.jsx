6eb8c4e0afeb8b9461296408d7444feef6674f1a
Eduardo Cano <canoeduardojesus@gmail.com>
2023-02-12 00:51:37 -0300

    stilos a la pagina de lista cripto

--- src/component/ListaCripto.jsx
+++ src/component/ListaCripto.jsx
@@ -6,22 +6,26 @@ export const ListaCripto = () => {
   const { data, error, loading } = metodoGET(API_URL)
 
   return (
-    <div className="row d-flex align-items-center justify-content-center">
-      <h1 className="col-12 text-center h1">Criptomonedas</h1>
-      {error && (<h1>Error en la petición</h1>)}
-      {loading && (<Loader />)}
+    <>
       {
-        data?.map(({ rank, id, name, priceUsd }) => (
-          <div key={id} className="card text-center mb-3 col-10 col-sm-4">
-            <div className="card-header">{rank}</div>
-            <div className="card-body">
-              <h5 className="card-title">{name}</h5>
-              <p className="card-text">{priceUsd}</p>
-              <a href="/" className="btn btn-primary">Detalles</a>
-            </div>
-          </div>
-        ))
+      loading ? (<Loader />)
+        : (<h1 className="text-center h1">Criptomonedas</h1>)
       }
-    </div>
+      {error && (<h1>Error en la petición</h1>)}
+      <div className="row d-flex align-items-center justify-content-center g-2 mt-">
+        {
+          data?.map(({ rank, id, name, priceUsd }) => (
+            <div key={id} className="card text-center mb-3 col-10 col-sm-2 m-4">
+              <div className="card-header">{rank}</div>
+              <div className="card-body">
+                <h5 className="card-title">{name}</h5>
+                <p className="card-text">{priceUsd}</p>
+                <a href="/" className="btn btn-outline-dark">Detalles</a>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 }
